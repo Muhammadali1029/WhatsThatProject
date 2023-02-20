819a53a93cea5002df8c3dc0003eec63e0896a81
Muhammadali1029 <104990048+Muhammadali1029@users.noreply.github.com>
2023-02-20 14:57:22 +0000

    login screen

---
+++ WhatsThatProject/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import LoginScreen from './components/login';
+import FlatListDemo from './components/flatlistdemo';
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <LoginScreen />
+        <text>Hello</text>
+      </View>
+      // <FlatListDemo />
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
